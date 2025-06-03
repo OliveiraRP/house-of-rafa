e534eadf9ff94b9b0786228abc81b367279d8767
Rafael Oliveira <oliveira.rp@outlook.pt>
2025-06-03 14:26:19 +0100

    Implement redirect to budget manager

--- client/src/components/AppListContainer.js
+++ client/src/components/AppListContainer.js
@@ -5,11 +5,15 @@ import AppIcon from "./AppIcon";
 import { APP_LINKS } from "../config/links";
 
 function AppListContainer() {
+  const token = localStorage.getItem("accessToken");
+
+  const budgetManagerRedirectUrl = `${APP_LINKS.budgetManager}?token=${token}`;
+
   return (
     <div className="app-list-container">
       <AppIcon
         icon={AiTwotoneEuro}
-        onClick={() => (window.location.href = APP_LINKS.budgetManager)}
+        onClick={() => (window.location.href = budgetManagerRedirectUrl)}
       />
     </div>
   );
